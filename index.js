45a7dc7a8521f10b113df2ef855db739384ca9f9
saihajvir <72048234+saihajvir@users.noreply.github.com>
2022-03-15 18:55:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: true
+  }
+});
+
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const allusers = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  // io.emit("change", socket.id)
+
+  allusers[socket.id] = {left: 0, top: 0};
+  io.emit("user_connected", allusers);
+
+  socket.on("alert_all", (text)=>{
+    io.emit("change", socket.id, text)
+  })
+  
+  socket.on("mouse_moved", (x,y)=> {
+    socket.broadcast.emit("change_mouse", x, y, socket.id);
+    //socket.broadcast.emit
+  })
+
+  socket.on("disconnect", () => {
+    delete allusers[socket.id];
+    io.emit("user_connected", allusers);
+  })
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
